8a72e4a6ac4a3b1ce1d713e627500c1a3054ab9f
Sanket_Jena <sanket.jena@bipros.com>
2024-04-23 13:23:02 +0530

    auth in role

--- api/v1/role.py
+++ api/v1/role.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
 
 from core import get_db
+from core.dependencies import token_verify_middleware
+from core.token import oauth2_scheme
 from crud import CRUDRoleInstance
 from schemas.role import RoleCreate
 
@@ -12,7 +14,7 @@ def get_all_role(db:Session = Depends(get_db)):
     roles = CRUDRoleInstance.get_all_record(db)
     return roles
 
-@role_router.post('/')
+@role_router.post('/',dependencies=[Depends(token_verify_middleware),Depends(oauth2_scheme)])
 def create_role(req:RoleCreate,db:Session=Depends(get_db)):
     new_role = CRUDRoleInstance.create(db,req)
     return new_role
